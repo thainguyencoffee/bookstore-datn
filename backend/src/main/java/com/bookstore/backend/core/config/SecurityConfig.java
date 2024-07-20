8ecf4ea505a305637089b9e76c511c2418a54222
thainguyencoffee <nguyennt11032004@gmail.com>
2024-07-20 22:22:30 +0700

    delete security config for /actuator

--- backend/src/main/java/com/bookstore/backend/core/config/SecurityConfig.java
+++ backend/src/main/java/com/bookstore/backend/core/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.bookstore.backend.core.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,8 +18,7 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/actuator/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/", "/api/reviews/**").permitAll()
                         .anyRequest().hasRole("employee")
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
